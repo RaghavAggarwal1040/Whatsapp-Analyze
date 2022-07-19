7fa0ed4bf8ee13f1581a9f4dc296cf2a4ea5ad1c
ALOK KUMAR AGGARWAL <cs19026@glbitm.ac.in>
2022-07-20 00:36:32 +0530

    make it better

--- app.py
+++ app.py
@@ -144,19 +144,19 @@ if uploaded_file is not None:
 
         #emoji_analysis
 
-        emoji_df=helper.emoji_helper(selected_user,df)
-        st.title("Emoji Analyzer")
-
-        col1,col2=st.columns(2)
-
-        with col1:
-            # st.figure(figsize=(18,10))
-            st.dataframe(emoji_df)
-
-        with col2:
-            fig,ax=plt.subplots()
-            ax.pie(emoji_df[1].head(),labels=emoji_df[0].head(),autopct="%0.2f")
-            st.pyplot(fig)
+        # emoji_df=helper.emoji_helper(selected_user,df)
+        # st.title("Emoji Analyzer")
+        #
+        # col1,col2=st.columns(2)
+        #
+        # with col1:
+        #     # st.figure(figsize=(18,10))
+        #     st.dataframe(emoji_df)
+        #
+        # with col2:
+        #     fig,ax=plt.subplots()
+        #     ax.pie(emoji_df[1].head(),labels=emoji_df[0].head(),autopct="%0.2f")
+        #     st.pyplot(fig)
 
         #sentiment analysis
 
